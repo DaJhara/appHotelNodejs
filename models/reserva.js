76f37a845c1bedb0bf1af3121f120708f7179161
DaJhara <dajara92@gmail.com>
2023-09-13 18:55:40 -0500

    se agregan campos a la clase reserva

--- models/reserva.js
+++ models/reserva.js
@@ -3,13 +3,16 @@ class Reserva {
     idCliente = '';
     fechaIngreso = '';
     dias = 0;
+    idHabitacion = ''; 
+    precio = 0; 
     servicios = [];
 
-    constructor(id, idCliente, fechaIngreso, dias, servicios) {
+    constructor(id, idCliente, fechaIngreso, dias, idHabitacion, servicios) {
         this.id = id;
         this.idCliente = idCliente;
         this.fechaIngreso = fechaIngreso;
         this.dias = dias;
+        this.idHabitacion = idHabitacion;
         this.servicios = servicios;
     }
 }
